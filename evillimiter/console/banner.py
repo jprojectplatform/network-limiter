c1737b3d1de7bac7580af4d382b6e297f04140b0
JMusicology | JHacker <108634137+jprojectplatform@users.noreply.github.com>
2024-01-05 06:24:51 -0500

    Update banner.py

--- evillimiter/console/banner.py
+++ evillimiter/console/banner.py
@@ -2,14 +2,16 @@ from .io import IO
 
 
 _MAIN_BANNER = r"""{}
-███████╗██╗   ██╗██╗██╗       ██╗     ██╗███╗   ███╗██╗████████╗███████╗██████╗ 
-██╔════╝██║   ██║██║██║       ██║     ██║████╗ ████║██║╚══██╔══╝██╔════╝██╔══██╗
-█████╗  ██║   ██║██║██║       ██║     ██║██╔████╔██║██║   ██║   █████╗  ██████╔╝
-██╔══╝  ╚██╗ ██╔╝██║██║       ██║     ██║██║╚██╔╝██║██║   ██║   ██╔══╝  ██╔══██╗
-███████╗ ╚████╔╝ ██║███████╗  ███████╗██║██║ ╚═╝ ██║██║   ██║   ███████╗██║  ██║
-╚══════╝  ╚═══╝  ╚═╝╚══════╝  ╚══════╝╚═╝╚═╝     ╚═╝╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
-                {}by bitbrute  ~  limit devices on your network :3
-                                    v[_V_]
+-------- 
+    !    
+    !      (◍•ᴗ•◍) 𝙃𝙖𝙘𝙠𝙚𝙧 💖彡
+    !    
+----!     
+
+                {}Edited by JHacker from J Project Platform  ~  limit devices on your network (LAN) 
+
+                            Telegram Join @JProjectPlatform
+                                    
 
 """.format(IO.Fore.LIGHTRED_EX, IO.Style.RESET_ALL + IO.Style.BRIGHT)
 
